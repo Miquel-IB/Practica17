9c7e031b2f94d59836d791bd91f66f37bc465211
Miquel-IB <57457918+Miquel-IB@users.noreply.github.com>
2020-11-24 17:46:22 +0100

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
+
+# Actualizamos e instalamos la aplicaciones de lamp y adminer
+
 apt-get update
 apt install -y apache2
 apt-get install -y php5 libapache2-mod-php5 php5-mysql
 sudo systemctl restart apache2
+
+# Agregamos el prorama adminer de github a nuestra página apache2
 cd /var/www/html
 wget https://github.com/vrana/adminer/releases/download/v4.3.1/adminer-4.3.1-mysql.php
 mv adminer-4.3.1-mysql.php adminer.php
 
+# En esta parte instalamos mysql-server y creamos un usuario con la contraseña root para poder acceder a mysql
 apt-get -y install debconf-utils
 
 DB_ROOT_PASSWD=root
@@ -15,6 +21,8 @@ debconf-set-selections <<< "mysql-server mysql-server/root_password_again passwo
 
 apt-get install -y mysql-server
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf
+
+# Reiniciamos el servicio de mysql
 /etc/init.d/mysql restart
 
 mysql -uroot mysql -p$DB_ROOT_PASSWD <<< "GRANT ALL PRIVILEGES ON *.* TO root@'%' IDENTIFIED BY '$DB_ROOT_PASSWD'; FLUSH PRIVILEGES;"
