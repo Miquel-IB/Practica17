c3ffbc2a3430d7af868cc5e62f84ef12f38cf63e
Miquel-IB <57457918+Miquel-IB@users.noreply.github.com>
2020-11-24 18:04:54 +0100

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+# https://github.com/Miquel-IB/Practica17.git
+
+nomscript=$0   # $0 es el nom del programa
+
+ 
+
+function usage () {
+
+   cat <<EOF
+
+Usage: nomscript [-u usuari] [-h hostname] [-t]
+
+   -u   usuario de la base da datos (obligatorio)
+
+   -h   hostname donde se conectara (obligatori)
+
+   -p   puerto 
+
+   -t   no se conecta,  solo comproba conexión
+
+EOF
+
+exit 1
+
+}
+
+
+
+ 
+
+while getopts ":u:h:p:t:" opt; do
+
+    case "${opt}" in
+
+        u)
+
+			u=${OPTARG}
+
+			;;
+
+        h)
+
+            h=${OPTARG}
+
+            ;;
+
+        p)
+
+            p=${OPTARG}
+
+            puerto=3306
+
+            if
+
+            [ ${OPTARG} -le "1024" || ${OPTARG} -gt "65535" ]; then
+
+				echo "No esta en el rango de puertos"
+
+			fi
+
+            ;;
+
+        t)
+
+            t=${OPTARG}
+
+            ;;
+
+        *)
+
+            usage
+
+            ;;
+
+    esac
+
+done
+
+shift $((OPTIND-1))
+
+ 
+
+# Check required switches exist
+
+if [ -z "${u}" ] || [ -z "${h}" ]; then
+
+	usage
+
+fi
